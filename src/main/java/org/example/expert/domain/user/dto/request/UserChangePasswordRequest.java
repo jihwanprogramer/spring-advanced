2d12a109fc9a0ad4626ef9c50ce14aeff2233bb6
jihwanprogramer <kob882333@naver.com>
2025-04-21 10:19:33 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,17 +1,24 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Validated
 public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = "(?=.*\\d.*)(?=.*[A-Z].*)",message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
