75ead607a66b44c12dab71efae908a11b444f7d1
jihwanprogramer <kob882333@naver.com>
2025-04-21 13:55:26 +0900

    레벨 5:회원가입 생성 테스트

---
+++ src/test/java/org/example/expert/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package org.example.expert.domain.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.expert.domain.auth.dto.request.SignupRequest;
+import org.example.expert.domain.auth.dto.response.SignupResponse;
+import org.example.expert.domain.auth.service.AuthService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthController.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void signup_success() throws Exception {
+
+        SignupRequest signupRequest = new SignupRequest("테스트@email.com", "1234", "USER");
+        SignupResponse signupResponse = new SignupResponse("비어토큰");
+
+        Mockito.when(authService.signup(any(SignupRequest.class))).thenReturn(signupResponse);
+
+
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.bearerToken").value("비어토큰"));
+    }
+}
